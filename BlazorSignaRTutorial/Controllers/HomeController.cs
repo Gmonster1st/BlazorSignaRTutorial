fe06e850421e9d833f41652642cb1c5870081bb7
Dimitrios Papazisis <gmonster1st@gmail.com>
2022-11-24 00:29:19 +0200

    Add project files.

---
+++ BlazorSignaRTutorial/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using BlazorSignaRTutorial.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlazorSignaRTutorial.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET: HomeController
+        [HttpGet("HomeController")]
+        public ActionResult Index()
+        {
+            return Ok();
+        }
+
+        // GET: HomeController/Details/5
+        [HttpGet("HomeController/Details/{id}")]
+        public async Task<ActionResult> Details(int id, [FromServices] MyTestProcess process)
+        {
+
+            var request = Request;
+
+            var baseUrl = string.Format("{0}://{1}/chathub", request.Scheme, request.Host);
+            await process.Start(baseUrl);
+
+            await process.SendAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ BlazorSignaRTutorial/Data/MyTestProcess.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorSignaRTutorial.Data
+{
+    public class MyTestProcess
+    {
+        private readonly NavigationManager navigationManager;
+        private HubConnection _hubConnection;
+        private bool open;
+
+        public MyTestProcess(NavigationManager navigationManager)
+        {
+            this.navigationManager = navigationManager;
+
+            //_hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+            //{
+            //    var encodedMsg = $"{user}: {message}";
+            //    messages.Add(encodedMsg);
+            //    InvokeAsync(StateHasChanged);
+            //});
+
+        }
+
+
+        public async Task<MyTestProcess> Start(string url)
+        {
+            _hubConnection = new HubConnectionBuilder().WithUrl(url).Build();
+
+            await _hubConnection.StartAsync();
+            open = true;
+            return this;
+        }
+
+        public async Task SendAsync(int num)
+        {
+            if (open)
+            {
+                await _hubConnection.SendAsync("SendMessage", "ControllerProcess", $"I am a Bot! and this is my Number {num}");
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+    }
+}
